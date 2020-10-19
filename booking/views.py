c30e9c14023df08533f81c8494511177e623cb82
Chuis <hdm8102000@gmail.com>
2020-10-19 22:23:03 +0700

    complete home page

--- booking/views.py
+++ booking/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
 def menu(request):
     return HttpResponse("menu")
